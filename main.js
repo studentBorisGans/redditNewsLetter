9e2471e304c8e1c81f050c20101ca183f9dc7eae
borisGansGit <bgans.ieu2023@student.ie.edu>
2024-04-09 18:12:13 +0200

    Top 5 Subreddits based on search

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 import './style.css'
+// import "redditData.json"
+
+let values;
+
+function fetchJSONData() {
+    fetch("redditData.json")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error
+                    (`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => 
+                // values = JSON.parse(data))
+                // console.log(data))
+                showSubReds(data))
+        .catch((error) => 
+               console.error("Unable to fetch data:", error));
+}
+fetchJSONData();
+console.log(values);
+
+function showSubReds(data) {
+    for (let i = 0; i < data.data.children.length; i++) {
+        console.log(data.data.children[i].data.display_name);
+    }
+}
+
+
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import json
+
 base_url = "https://www.reddit.com/"
 data = {"grant_type": "password", "username": "epicgooseman", "password": "MadridMarathon27"}
 auth = requests.auth.HTTPBasicAuth("yhGCHuRDluWwH9Fq7Tg2TQ", "aNAKWOFVX5ET5yHtN2CzBu2zVUAAWQ")
@@ -19,7 +21,7 @@ response = requests.get(base_url + '/api/v1/me', headers=headers)
 if response.status_code == 200:
     print(response.json()['name'], response.json()['comment_karma'])
 
-payload = {'q': 'donald trump', 'limit': 5, 'sort': 'relevance'}
+payload = {'q': 'cats', 'limit': 5, 'sort': 'relevance'}
 response = requests.get(base_url + '/subreddits/search', headers=headers, params=payload)
 print(response.status_code)
 
@@ -29,3 +31,8 @@ values = response.json()
 for i in range(len(values['data']['children'])):
     print(values['data']['children'][i]['data']['display_name'])
 
+with open("redditData.json", "w") as file:
+    json.dump(values, file)
+
+
+# /api/v1/collections/collection
